0e0b2450b31969793cbfd13d73ec2dbedc459773
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-10-26 16:03:16 +0100

    At this point I need to change a bunge of names for thing because they are very different to the code alongs and have caused a lot of confusion

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -4,10 +4,12 @@ const User = require("../user/model");
 
 const saltRounds = parseInt(process.env.SALT_ROUNDS);
 
+const jwt = require("jsonwebtoken");
+
 const hashPass = async (req, res, next) => {
   try {
     req.body.password = await bcrypt.hash(req.body.password, saltRounds);
-    console.log(hashPass);
+    console.log("hashPass", hashPass);
     next();
   } catch (error) {
     res.status(501).json({ errormessage: error.message, error });
@@ -16,16 +18,19 @@ const hashPass = async (req, res, next) => {
 
 const comparePass = async (req, res, next) => {
   try {
-    const user = await User.findOne({ where: { userName: req.body.userName } });
-    if (!user) {
+    req.user = await User.findOne({ where: { userName: req.body.userName } });
+    console.log("from compare pass", req.user);
+    if (!req.user) {
       res.status(401).json({ message: "Invalid Username" });
       return;
     }
     const unhashedPassword = await bcrypt.compare(
       req.body.password,
-      user.password
+      req.user.password
     );
-    req.unhashedPassword = unhashedPassword;
+    if (!unhashedPassword) {
+      res.send("not authorized");
+    }
     next();
 
     //find the user
@@ -38,4 +43,26 @@ const comparePass = async (req, res, next) => {
   }
 };
 
-module.exports = { hashPass, comparePass };
+//tokenCheck
+
+let otherToken = "Random string";
+
+const tokenCheck = async (req, res, next) => {
+  try {
+    let generatedToken = req.header("Authorization").replace("Bearer ", "");
+
+    const decodedToken = await jwt.verify(
+      generatedToken,
+      process.env.SECRET_KEY
+    );
+    req.user = await User.findOne({ where: { id: decodedToken.id } });
+    req.user = await User.findOne({ where: { id: 1 } });
+    console.log(req.user);
+    console.log("Valid Token");
+    next();
+  } catch {
+    console.log("Invalid Token");
+  }
+};
+
+module.exports = { hashPass, comparePass, tokenCheck };
--- src/user/routes.js
+++ src/user/routes.js
@@ -3,13 +3,15 @@ const userRouter = Router();
 
 const User = require("./model");
 
-const { hashPass, comparePass } = require("../middleware/index");
+const { hashPass, comparePass, tokenCheck } = require("../middleware/index");
 const { registerUser, findAllUsers, loginUser } = require("./controllers");
 
 userRouter.post("/", hashPass, registerUser);
 
-userRouter.get("/", findAllUsers);
+userRouter.get("/", tokenCheck, findAllUsers);
 
 userRouter.post("/login", comparePass, loginUser);
+
+userRouter.get("/authCheck", tokenCheck, loginUser);
 
 module.exports = userRouter;
